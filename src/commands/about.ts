d3b6011e9bc6da185637f0189eb1d8be0bc0d176
Gustavo Rivero <gustavoerivero12@gmail.com>
2023-06-07 17:11:19 -0400

    Update about command

--- src/commands/about.ts
+++ src/commands/about.ts
@@ -3,10 +3,12 @@ import createDebug from 'debug'
 
 import { author, name, version, homepage } from '../../package.json'
 
+const botName = process.env.BOT_NAME
+
 const debug = createDebug('bot:about_command')
 
 const about = () => async (ctx: Context) => {
-  const message = `*${name} ${version}\n${homepage}*\n*Author: ${author}*`
+  const message = `*${botName}*\n${name} ${version}\n${homepage}\n_Autor: ${author}_`
   debug(`Triggered "about" command with message \n${message}`)
   await ctx.replyWithMarkdownV2(message, { parse_mode: 'Markdown' })
 }
