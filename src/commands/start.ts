b8d982dbc1d897917c5dc8bafd934393cea50f77
Gustavo Rivero <gustavoerivero12@gmail.com>
2023-06-07 12:03:28 -0400

    Update start command

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -8,7 +8,7 @@ const debug = createDebug('bot:about_command')
 export const start = () => async (ctx: Context) => {
 
   const username = `${ctx.message?.from.first_name} ${ctx.message?.from.last_name}`
-  const message = `¡Hola ${username}! A continuación te muestro las cosas que puedo hacer:\n${menu}\nAsí que dime, ¿qué puedo hacer por ti?`
+  const message = `¡Hola ${username}! A continuación te muestro las cosas que puedo hacer:\n${menu()}\nAsí que dime, ¿qué puedo hacer por ti?`
 
   debug(`Triggered "start" command with message \n${message}`)
   
