5fb2b8edd3d7a21bf914cd6583b2ab5ea7dc9dbd
Gustavo Rivero <gustavoerivero12@gmail.com>
2023-06-09 11:07:58 -0400

    Update vercel cron

--- api/index.ts
+++ api/index.ts
@@ -1,12 +1,9 @@
 import { VercelRequest, VercelResponse } from '@vercel/node'
-import { cronVercel, startVercel } from '../src'
+import { startVercel } from '../src'
 
 export default async function handle(req: VercelRequest, res: VercelResponse) {
   try {
     await startVercel(req, res)
-    if (req.url === '/api/cron') {
-      cronVercel()
-    }
   } catch (e: any) {
     res.statusCode = 500
     res.setHeader('Content-Type', 'text/html')
