43af6b0598e2196677335f005de4ee68816daaca
Gustavo Rivero <gustavoerivero12@gmail.com>
2023-06-09 10:34:13 -0400

    Update vercel cron

--- src/core/production.ts
+++ src/core/production.ts
@@ -31,7 +31,12 @@ const production = async (
   if (req.method === 'POST') {
     await bot.handleUpdate(req.body as unknown as Update, res)
   } else {
-    res.status(200).json('Listening to bot events...')
+    let url = ''
+
+    if (req.url === '/api/cron') {
+      url = '\nAnd activate cronjob.'
+    }
+    res.status(200).json(`Listening to bot events...${req.url}`)
   }
   debug(`starting webhook on port: ${PORT}`)
 }
