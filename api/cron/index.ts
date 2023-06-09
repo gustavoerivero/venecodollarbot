c4e9c0b6c5fdeacbe0ada0a4a7aef2fab5c4cc86
Gustavo Rivero <gustavoerivero12@gmail.com>
2023-06-09 11:30:40 -0400

    Update vercel cron

--- api/cron/index.ts
+++ api/cron/index.ts
@@ -1,10 +1,10 @@
 import { VercelRequest, VercelResponse } from '@vercel/node'
-import { cronVercel, startVercel } from '../src'
+import { cronVercel, startVercel } from '../../src'
 
 export default async function handle(req: VercelRequest, res: VercelResponse) {
   try {
-    await startVercel(req, res)
-    cronVercel()
+    res.setHeader('Content-Type', 'text/html')
+    res.end('<h1>This is the route to the cronjob</h1><p>Well, that it is.</p>')
   } catch (e: any) {
     res.statusCode = 500
     res.setHeader('Content-Type', 'text/html')
