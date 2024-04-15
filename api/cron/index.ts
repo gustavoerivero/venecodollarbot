cef8302c94c5f986a1761d3da7cbadc21222e833
gustavoerivero <gustavoerivero12@gmail.com>
2024-04-15 11:30:43 -0400

    Update cron settings

--- api/cron/index.ts
+++ api/cron/index.ts
@@ -3,11 +3,24 @@ import { cronVercel } from "../../src";
 
 export default async function handle(req: VercelRequest, res: VercelResponse) {
   try {
+
+    const authHeader = req.headers.authorization;
+
+    const { CRON_SECRET } = process.env;
+
+    if (!CRON_SECRET || authHeader !== `Bearer ${CRON_SECRET}`) {
+      return res
+        .status(401)
+        .end("<h1>Authorization Error</h1><p>Sorry, there was a problem with the authorization cronjob.</p>");
+    }
+
     const users = await cronVercel();
     console.info("Active users:", users);
+
   } catch (e: any) {
-    res.statusCode = 500;
-    res.end("<h1>Server Error</h1><p>Sorry, there was a problem with the cronjob.</p>");
+    res
+      .status(500)
+      .end("<h1>Server Error</h1><p>Sorry, there was a problem with the cronjob.</p>");
     console.error(e.message);
   }
 };
